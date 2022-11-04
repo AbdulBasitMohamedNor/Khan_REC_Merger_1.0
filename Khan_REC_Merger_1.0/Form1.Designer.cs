994e6d8bf1dc0d87c74b6242be6ab57cf3f9e39f
Basit Mohamednor <Basit.Mohamednor@spectrogon.com>
2022-11-04 13:12:53 +0100

    First

---
+++ Khan_REC_Merger_1.0/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Khan_REC_Merger_1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.MERGE = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Source_Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OriginalFilesFolder);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Destination_Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DestinationFIlesFolder);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(210, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(566, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(210, 205);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(566, 31);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(210, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(566, 31);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Data_File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.XDataFile);
+            // 
+            // MERGE
+            // 
+            this.MERGE.Location = new System.Drawing.Point(290, 285);
+            this.MERGE.Name = "MERGE";
+            this.MERGE.Size = new System.Drawing.Size(195, 92);
+            this.MERGE.TabIndex = 6;
+            this.MERGE.Text = "MERGE";
+            this.MERGE.UseVisualStyleBackColor = true;
+            this.MERGE.Click += new System.EventHandler(this.MERGE_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(268, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 54);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "REC_Merger";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MERGE);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button MERGE;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Khan_REC_Merger_1.0/Form1.cs
@@ -0,0 +1,174 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+/*
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+*/
+namespace Khan_REC_Merger_1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            
+        }
+      
+        private void OriginalFilesFolder(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                string folder = Directory.GetCurrentDirectory().ToString();
+
+                string directoryPath = Path.GetDirectoryName(fbd.SelectedPath);
+
+                this.textBox1.Text = fbd.SelectedPath;
+            }
+
+        }        
+        
+        private void DestinationFIlesFolder(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                string folder = Directory.GetCurrentDirectory().ToString();
+
+                string directoryPath = Path.GetDirectoryName(fbd.SelectedPath);
+
+                this.textBox3.Text = fbd.SelectedPath;
+
+            }
+        }
+        private void XDataFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "";
+            openFileDialog1.Filter = "Khan files (*.REC, *.json)|*.REC;*.json";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog1.FileName;
+                this.textBox2.Text = selectedFileName;
+            }
+        }
+
+        //****************************************************
+        // Mergar REC. json Obj eller array i en flera REC. Json filer
+        // IN: Json Obj
+        // UT: Nya Updaterade Json filer
+        //****************************************************
+        void MergeFiles()
+        {
+            int i = 1;
+           
+            //Att göra; Use rich file Box. paste Json Object
+            var updateFilePath = String.Format($"{this.textBox2.Text}", AppDomain.CurrentDomain.BaseDirectory);
+            string jsonUpdateFile = new StreamReader(updateFilePath).ReadToEnd();
+            var jsonU = JObject.Parse(jsonUpdateFile);
+
+            //save to file
+
+
+
+            //Att göra; Use adressbox to anable the user to choose path.  // jobbDAtorn : C:\Users\basmoh\source\repos\JsonMerger\JsonMerger\bin\Debug\netcoreapp3.1
+            //hemma  Parallel.ForEach(Directory.EnumerateFiles(@"C:\Users\abuam\source\repos\ConsoleIronXL\ConsoleIronXL\bin\Debug\net5.0\json", "*.json"), (file) =>
+            Parallel.ForEach(Directory.EnumerateFiles($"{this.textBox1.Text}", "*.REC"), (file) =>
+            {
+
+              
+
+                string filename = file.Substring(file.LastIndexOf(@"\") -1);
+               
+
+                // Välj Mapp
+                var OldFilePath = String.Format(file, AppDomain.CurrentDomain.BaseDirectory);
+                string jsonOldFile = new StreamReader(OldFilePath).ReadToEnd();
+                var jsonO = JObject.Parse(jsonOldFile);
+
+
+                //merge new json into old json
+                jsonO.Merge(jsonU);
+
+                var NewFile = String.Format($"{filename}" , AppDomain.CurrentDomain.BaseDirectory);
+
+                i++;
+
+                FileInfo fileToSave = new FileInfo(NewFile);
+                fileToSave.Directory.Create();
+                string JsonToSave = JsonConvert.SerializeObject(jsonO);
+                System.IO.File.WriteAllText(@NewFile, JsonToSave);
+                NewFile = "";
+            });
+        }
+
+
+        private void MERGE_Click(object sender, EventArgs e)
+        {
+            int i = 1;
+
+            //Att göra; Use rich file Box. paste Json Object
+            var updateFilePath = String.Format(this.textBox2.Text, AppDomain.CurrentDomain.BaseDirectory);
+            string jsonUpdateFile = new StreamReader(updateFilePath).ReadToEnd();
+            var jsonU = JObject.Parse(jsonUpdateFile);
+
+            //save to file
+
+
+
+            //Att göra; Use adress box to anable the user to choose path.  // jobbDAtorn : C:\Users\basmoh\source\repos\JsonMerger\JsonMerger\bin\Debug\netcoreapp3.1
+            //hemma  Parallel.ForEach(Directory.EnumerateFiles(@"C:\Users\abuam\source\repos\ConsoleIronXL\ConsoleIronXL\bin\Debug\net5.0\json", "*.json"), (file) =>
+            Parallel.ForEach(Directory.EnumerateFiles(this.textBox1.Text, "*.REC"), (file) =>
+            {
+
+
+
+                string filename = file.Substring(file.LastIndexOf("\\") - 0);
+
+
+                var OldFilePath = String.Format(file, AppDomain.CurrentDomain.BaseDirectory);
+                string jsonOldFile = new StreamReader(OldFilePath).ReadToEnd();
+                var jsonO = JObject.Parse(jsonOldFile);
+
+
+                //merge new json into old json
+                jsonO.Merge(jsonU);
+
+                var NewFile = String.Format(this.textBox3.Text + filename, AppDomain.CurrentDomain.RelativeSearchPath);
+
+                i++;
+
+                FileInfo fileToSave = new FileInfo(NewFile);
+                fileToSave.Directory.Create();
+                string JsonToSave = JsonConvert.SerializeObject(jsonO);
+                System.IO.File.WriteAllText(@NewFile, JsonToSave);
+                NewFile = "";
+            });
+
+            MessageBox.Show(" KLART! ");
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MergeFiles();
+        }
+    }
+}
